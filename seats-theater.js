34638821ac0d0f5d59729867f3f3db999803d47d
gdkparadox <paradoxsmurf@outlook.com>
2020-08-13 19:00:27 +0100

    asd

--- seats-theater.js
+++ seats-theater.js
@@ -14,4 +14,4 @@ console.log(seatsInTheater(16,11,5,3) , 96)
 console.log(seatsInTheater(1,1,1,1) , 0)
 console.log(seatsInTheater(13,6,8,3) , 18)
 console.log(seatsInTheater(60,100,60,1) , 99)
-console.log(seatsInTheater(1000,1000,1000,1000) , 0)
+console.log(seatsInTheater(1000,1000,1000,1000) , 0) //f
